324c9e2ce29d752367e69ed72707ea1fb7ca9bfa
hwp07 <hongphongpham2108@gmail.com>
2026-07-08 23:38:52 +0700

    Add fike

--- SS11/database.py
+++ SS11/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:21082007@localhost:3306/trackingnumber"
+DATABASE_URL = "mysql+pymysql://root:21082007@localhost:3306/parking_management"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-    autoflush=False,
     autocommit=False,
+    autoflush=False,
     bind=engine
 )
 
