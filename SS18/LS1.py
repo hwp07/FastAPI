f8c513c96c8d9bd3877c2d61c19841a39ee6f439
hwp07 <hongphongpham2108@gmail.com>
2026-07-16 20:48:06 +0700

    Add file

--- SS18/LS1.py
+++ SS18/LS1.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import (
     Session
 )
 
-DATABASE_URL = “sqlite:///./company.db”
+DATABASE_URL = "sqlite:///./company.db"
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
